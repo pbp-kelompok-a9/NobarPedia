1a02e81e0cbf89df6555bc662eaa3849a5074061
daffa i <daffa2.ismail178@gmail.com>
2025-10-21 21:49:21 +0700

    curd works, errors fixed

--- join/urls.py
+++ join/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from join.views import tes, post_join, get_join_list, get_join_record, update_join, delete_join
+from join.views import show_tes, post_join, get_join_list, get_join_record, update_join, delete_join
 
 app_name = 'join'
 
 urlpatterns = [
-    path('', tes),
-    path('post/', post_join),
-    path('get/', get_join_list),
-    path('get/<uuid:id>/', get_join_record),
-    path('update/<uuid:id>/', update_join),
-    path('delete/<uuid:id>/', delete_join),
+    path('', show_tes, name='show_tes'),
+    path('post/', post_join, name='post_join'),
+    path('get/', get_join_list, name='get_join_list'),
+    path('get/<uuid:id>/', get_join_record, name='get_join_record'),
+    path('update/<uuid:id>/', update_join, name='update_join'),
+    path('delete/<uuid:id>/', delete_join, name='delete_join'),
 ]
