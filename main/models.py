cd97647ee1d711f36aac18969674e27711f427a0
nvae <88143635+evan052006@users.noreply.github.com>
2025-10-21 21:14:20 +0700

    Added initial_dataset

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class NobarLocation(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=8, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    maps_url = models.URLField()
+    stars = models.SmallIntegerField()
+    
--- nobarpedia/urls.py
+++ nobarpedia/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+app_name = 'main'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
