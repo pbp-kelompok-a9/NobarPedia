c17a3c576ac53b1e18b1255157b85c10c806ed87
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2025-10-24 02:12:12 +0700

    style register page

--- account/forms.py
+++ account/forms.py
@@ -3,27 +3,48 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
+# class CustomUserCreationForm(UserCreationForm):
+#     # email = forms.EmailField(required=True)
+#     fullname = forms.CharField(max_length=100, required=True)
+#     bio = forms.CharField(widget=forms.Textarea, required=False)
+#     profile_picture = forms.ImageField(required=False) 
+    
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password1', 'password2', 'fullname', 'bio', 'profile_picture']
+
+#     def save(self, commit=True):
+#         user = super().save(commit=False)
+#         user.email = self.cleaned_data['email']  # Store email ke model user
+#         if commit:
+#             user.save()
+#             # Bikin object Profile baru, store data profil disana, sambungkan sama model user tadi
+#             profile = Profile.objects.create(
+#                 user=user,
+#                 fullname=self.cleaned_data.get('fullname'),
+#                 bio=self.cleaned_data.get('bio'),
+#                 profile_picture=self.cleaned_data.get('profile_picture', None) 
+#             )
+#             profile.save()
+#         return user
+
 class CustomUserCreationForm(UserCreationForm):
     # email = forms.EmailField(required=True)
-    fullname = forms.CharField(max_length=100, required=True)
-    bio = forms.CharField(widget=forms.Textarea, required=False)
-    profile_picture = forms.ImageField(required=False) 
+    #fullname = forms.CharField(max_length=100, required=True)
+    #bio = forms.CharField(widget=forms.Textarea, required=False)
+    #profile_picture = forms.ImageField(required=False) 
     
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'fullname', 'bio', 'profile_picture']
+        fields = ['username', 'password1', 'password2']
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.email = self.cleaned_data['email']  # Store email ke model user
         if commit:
             user.save()
             # Bikin object Profile baru, store data profil disana, sambungkan sama model user tadi
             profile = Profile.objects.create(
                 user=user,
-                fullname=self.cleaned_data.get('fullname'),
-                bio=self.cleaned_data.get('bio'),
-                profile_picture=self.cleaned_data.get('profile_picture', None) 
             )
             profile.save()
         return user
