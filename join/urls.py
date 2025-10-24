9daa7f9c5fb73336ffe58b57525967f0646da082
daffa i <daffa2.ismail178@gmail.com>
2025-10-24 16:42:18 +0700

    urls.py update

--- join/urls.py
+++ join/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from join.views import show_join, post_join, get_join_list, get_join_record, update_join, delete_join
+from join.views import show_join, post_join, get_join, update_join, delete_join
 
 app_name = 'join'
 
 urlpatterns = [
     path('', show_join, name='show_join'),
     path('post/', post_join, name='post_join'),
-    path('get/', get_join_list, name='get_join_list'),
-    path('get/<uuid:id>/', get_join_record, name='get_join_record'),
+    path('get/', get_join, name='get_join'),
     path('update/<uuid:id>/', update_join, name='update_join'),
     path('delete/<uuid:id>/', delete_join, name='delete_join'),
 ]
