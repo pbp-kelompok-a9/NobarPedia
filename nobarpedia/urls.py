60710cf24d546e3174a1e65b19a72f487af9893f
daffa i <daffa2.ismail178@gmail.com>
2025-10-24 18:32:42 +0700

    merge fix, added review url routing

--- nobarpedia/urls.py
+++ nobarpedia/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('join/', include('join.urls')),
+    path('review/', include('review.urls')),
 ]
